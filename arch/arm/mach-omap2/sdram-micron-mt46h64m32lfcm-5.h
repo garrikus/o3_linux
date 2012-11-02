4835bf44b1b649d83e2bcc8e74f18ab834d35907
Igor S.K <garrik@navis.ru>
2012-11-02 14:41:50 +0300

    o2: Add mt46h64m32lfcm-5 DDR timing support.

---
+++ arch/arm/mach-omap2/sdram-micron-mt46h64m32lfcm-5.h
@@ -0,0 +1,55 @@
+/*
+ * SDRC register values for the Micron MT46H64M32LFCM-5
+ *
+ *
+ *
+ *
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef ARCH_ARM_MACH_OMAP2_SDRAM_MICRON_MT46H64M32LFCM
+#define ARCH_ARM_MACH_OMAP2_SDRAM_MICRON_MT46H64M32LFCM
+
+#include <plat/sdrc.h>
+
+/* Micron MT46H64M32LFCM-5 */
+/* XXX Using ARE = 0x1 (no autorefresh burst) -- can this be changed? */
+static struct omap_sdrc_params mt46h64m32lfcm5_sdrc_params[] = {
+	[0] = {
+		.rate	     = 200000000,
+		.actim_ctrla = 0x7ae1b4c6,
+		.actim_ctrlb = 0x00021217,
+		.rfr_ctrl    = 0x000bfe01,
+		.mr	     = 0x00000032,
+	},
+	[1] = {
+		.rate	     = 185000000,
+		.actim_ctrla = 0x72e1b4c6,
+		.actim_ctrlb = 0x00021215,
+		.rfr_ctrl    = 0x000b1601,
+		.mr	     = 0x00000032,
+	},
+	[2] = {
+		.rate	     = 166000000,
+		.actim_ctrla = 0x629db4c6,
+		.actim_ctrlb = 0x00011113,
+		.rfr_ctrl    = 0x0009f601,
+		.mr	     = 0x00000032,
+	},
+	[3] = {
+		.rate	     = 133000000,
+		.actim_ctrla = 0x5259b485,
+		.actim_ctrlb = 0x0001110f,
+		.rfr_ctrl    = 0x0007ee01,
+		.mr	     = 0x00000032,
+	},
+	[4] = {
+		.rate	     = 0
+	},
+};
+
+#endif
