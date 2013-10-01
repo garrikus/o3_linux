e9aa9ea9a324a3728b25b466b6108a58f599900e
Igor S.K <garrik@navis.ru>
2013-10-01 11:36:52 +0400

    Revert "o2: Show omapfb test pattern only once -- on startup."
    This reverts commit 75112b87da5cb7d6047f1271af5e21654936c342.

--- drivers/video/omap2/omapfb/omapfb-main.c
+++ drivers/video/omap2/omapfb/omapfb-main.c
@@ -971,7 +971,6 @@ err:
 	return r;
 }
 
-static int pattern_shown = 0;
 /* apply var to the overlay */
 int omapfb_apply_changes(struct fb_info *fbi, int init)
 {
@@ -984,11 +983,8 @@ int omapfb_apply_changes(struct fb_info *fbi, int init)
 	int i;
 
 #ifdef DEBUG
-	if (omapfb_test_pattern && !pattern_shown)
-    {
+	if (omapfb_test_pattern)
 		fill_fb(fbi);
-        pattern_shown++;
-    }
 #endif
 
 	WARN_ON(!atomic_read(&ofbi->region->lock_count));
